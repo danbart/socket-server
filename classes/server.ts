368f346a8cdc0719749853199e3850139c7b90b1
danbart <danilo_solorzano06@hotmail.com>
2020-01-10 10:29:53 -0600

    Seccion 6 lista

--- classes/server.ts
+++ classes/server.ts
@@ -39,7 +39,7 @@ export default class Server {
             // console.log('Cliente Conectado');
 
             // conectar cliente
-            socket.conectarCliente( cliente );
+            socket.conectarCliente( cliente, this.io );
             
             //Configurar usuario
             socket.configurarUsuario(cliente, this.io );
@@ -48,7 +48,10 @@ export default class Server {
             socket.mensaje(cliente, this.io );
 
             // Desconectar
-            socket.desconectar(cliente);
+            socket.desconectar(cliente, this.io);
+
+            // Obtener usuarios
+            socket.obtenerUsuarios( cliente, this.io );
 
         })
 
--- sockets/socket.ts
+++ sockets/socket.ts
@@ -5,16 +5,18 @@ import { Usuario } from '../classes/usuario';
 
 export const usuariosConectados = new UsuariosLista();
 
-export const conectarCliente = ( cliente: Socket ) {
+export const conectarCliente = ( cliente: Socket, io:socketIO.Server ) => {
     const usuario = new Usuario( cliente.id );
     usuariosConectados.agregar( usuario );
+    
 }
 
-export const desconectar = (cliente: Socket) => {
+export const desconectar = (cliente: Socket, io: socketIO.Server ) => {
     
     cliente.on('disconnect', () => {
         console.log('Cliente desconectado');
         console.log(usuariosConectados.borrarUsuario(cliente.id));
+        io.emit('usuarios-activos', usuariosConectados.getLista());
     })
 }
 
@@ -36,9 +38,21 @@ export const configurarUsuario = (cliente: Socket, io: socketIO.Server ) => {
 
         usuariosConectados.actualizarNombre(cliente.id, payload.nombre);
 
+        io.emit('usuarios-activos', usuariosConectados.getLista());
+
         callback({
             ok: true,
             mensaje: `Usuario ${payload.nombre} Configurado`
         });
     } )
+}
+
+// Obtener Usuarios
+export const obtenerUsuarios = (cliente: Socket,  io: socketIO.Server ) => {
+
+    cliente.on('obtener-usuarios', () => {
+        // Mandamos la información a la persona que lo esta solicitando
+        io.to( cliente.id ).emit('usuarios-activos', usuariosConectados.getLista());
+    })
+
 }
